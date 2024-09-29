bf0bb831c0a1e12f232e82a927947eb4a113e1b4
Eddielin9876 <75624242+onetwothreefourfivesixe@users.noreply.github.com>
2024-09-29 13:43:05 -0400

    Update responses.py

--- responses.py
+++ responses.py
@@ -4,4 +4,4 @@ import fetchQuestions
 
 def get_response(message: Message, userInput: str) -> str:
     lowered: str = userInput.lower()
-    return 'This was not a command. To use a command, type !help to see all commands. Also, you ' + choice(["You smell.", "cringe."])
+    return 'This was not a command. To use a command, type !help to see all commands. Also, you ' + choice(["smell.", "cringe."])
